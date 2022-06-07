8d89a360685c899065bc9fc4ea6b6034d9715a31
吴锋 <wufeng@wufengdeMacBook-Air.local>
2022-06-07 20:09:14 +0800

    mac icon size

---
+++ MauiTookit/Source/Maui.Toolkit/Platforms/MacCatalyst/Helpers/RuntimeHelper.cs
@@ -0,0 +1,18 @@
+﻿using Foundation;
+using Maui.Toolkit.Platforms.MacCatalyst.Runtimes;
+using ObjCRuntime;
+using System;
+namespace Maui.Toolkit.Platforms.MacCatalyst.Helpers;
+
+public static class RuntimeHelper
+{
+    public static NSObject? Alloc(string name)
+    {
+        var allocSelector = new Selector("alloc");
+        var intPtr = RuntimeInterop.IntPtr_objc_msgSend(Class.GetHandle(name), allocSelector.Handle);
+        if (intPtr == IntPtr.Zero)
+            return default;
+
+        return Runtime.GetNSObject(intPtr);
+    }
+}
