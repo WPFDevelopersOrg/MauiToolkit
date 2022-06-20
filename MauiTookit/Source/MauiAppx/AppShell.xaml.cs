aa5f1ad180c5bce21fca3bf8f09f99ad5e946066
MicroSugar <wufeng4728704@hotmail.com>
2022-06-20 16:37:41 +0800

    mobile add loginpage

--- MauiTookit/Source/MauiAppx/AppShell.xaml.cs
+++ MauiTookit/Source/MauiAppx/AppShell.xaml.cs
@@ -1,9 +1,12 @@
-﻿namespace MauiAppx;
+﻿using MauiAppx.Views;
+
+namespace MauiAppx;
 
 public partial class AppShell : Shell
 {
     public AppShell()
     {
         InitializeComponent();
+        Routing.RegisterRoute("LoginRouter", typeof(LoginPage));
     }
 }
