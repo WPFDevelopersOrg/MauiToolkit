3ee0ee34664aecf650f77cc9df5ecca387fbe297
MicroSugar <wufeng4728704@hotmail.com>
2022-06-27 00:00:46 +0800

    add notifications

---
+++ MauiTookit/Source/Maui.Toolkit/Platforms/MacCatalyst/Notifications/NsAppNotifications.cs
@@ -0,0 +1,19 @@
+﻿using Foundation;
+
+namespace Maui.Toolkit.Platforms.MacCatalyst.Notifications;
+
+public static class NsApplication_Notifications
+{
+    public static NSObject ObserveWillBecomeActive(NSObject objectToObserve, EventHandler<NSNotificationEventArgs> handler)
+    {
+        
+        EventHandler<NSNotificationEventArgs> handler2 = handler;
+
+        var naApplicationWillBecomeActive = Dlfcn.GetStringConstant(Libraries.AppKit.Handle, "NSApplicationWillBecomeActiveNotification");
+
+        return NSNotificationCenter.DefaultCenter.AddObserver(naApplicationWillBecomeActive, delegate (NSNotification notification)
+        {
+            handler2(null, new NSNotificationEventArgs(notification));
+        });
+    }
+}
