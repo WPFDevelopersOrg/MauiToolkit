d4543913b105e1f49324d56947dbcb003555ea04
MicroSugar <wufeng4728704@hotmail.com>
2022-06-28 10:25:58 +0800

    fixed

--- MauiTookit/Source/Maui.Toolkitx/WindowChrome.cs
+++ MauiTookit/Source/Maui.Toolkitx/WindowChrome.cs
@@ -3,7 +3,7 @@
 public partial class WindowChrome
 {
     public static readonly BindableProperty WindowChromeProperty =
-                       BindableProperty.CreateAttached("WindowChrome", typeof(WindowChrome), typeof(AppTitleBarExProperty), default, propertyChanged: WindowChromePropertyChanged);
+                       BindableProperty.CreateAttached("WindowChrome", typeof(WindowChrome), typeof(WindowChrome), default, propertyChanged: WindowChromePropertyChanged);
 
     public static WindowChrome GetWindowChrome(BindableObject target) => (WindowChrome)target.GetValue(WindowChromeProperty);
     public static void SetWindowChrome(BindableObject target, WindowChrome value) => target.SetValue(WindowChromeProperty, value);
