c6a5549603a4e7c00fdd324216d88999b5050b18
吴锋 <wufeng4728704@hotmainl.com>
2022-07-16 01:22:11 +0800

    bug

--- MauiTookit/Source/Maui.Toolkitx/Assists/VisualElementAssists.cs
+++ MauiTookit/Source/Maui.Toolkitx/Assists/VisualElementAssists.cs
@@ -4,7 +4,7 @@ namespace Maui.Toolkitx.Assists;
 public partial class VisualElementAssists
 {
     public static readonly BindableProperty AcrylicBrushProperty =
-                           BindableProperty.CreateAttached("AcrylicBrush", typeof(VisualElementAssists), typeof(MauiAcrylicBrush), default, propertyChanged: AcrylicBrushPropertyChanged);
+                           BindableProperty.CreateAttached("AcrylicBrush", typeof(MauiAcrylicBrush), typeof(VisualElementAssists), default, propertyChanged: AcrylicBrushPropertyChanged);
 
     public static MauiAcrylicBrush GetAcrylicBrush(VisualElement target) => (MauiAcrylicBrush)target.GetValue(AcrylicBrushProperty);
     public static void SetAcrylicBrush(VisualElement target, MauiAcrylicBrush value) => target.SetValue(AcrylicBrushProperty, value);
