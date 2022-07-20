148a9d370a7eb51ffb601e12a8e828daefa9ff9a
ChisterWu <wufeng4728704@hotmail.com>
2022-07-21 00:06:55 +0800

    add

--- MauiTookit/Source/Maui.Toolkitx/Platforms/MacCatalyst/WindowStartupService.cs
+++ MauiTookit/Source/Maui.Toolkitx/Platforms/MacCatalyst/WindowStartupService.cs
@@ -1,17 +1,22 @@
 ﻿using Microsoft.Maui.Platform;
+using UIKit;
 
 namespace Maui.Toolkitx;
 
 // All the code in this file is only included on Windows.
-internal partial class WindowStartupService : IWindowStartupService, IService
+internal partial class WindowStartupService : IService
 {
     public WindowStartupService(Window window, WindowStartup windowStartup)
     {
         ArgumentNullException.ThrowIfNull(window);
         ArgumentNullException.ThrowIfNull(windowStartup);
+        if (window.Handler?.PlatformView is not UIWindow uiWindow)
+            ArgumentNullException.ThrowIfNull(nameof(UIWindow));
+        else
+            _PlatformWindow = uiWindow;
+
         _Window = window;
         _WindowStartup = windowStartup;
-
     }
 
     public WindowStartupService(Window window, IElementHandler handler, WindowStartup windowStartup)
@@ -19,17 +24,24 @@ internal partial class WindowStartupService : IWindowStartupService, IService
         ArgumentNullException.ThrowIfNull(window);
         ArgumentNullException.ThrowIfNull(handler);
         ArgumentNullException.ThrowIfNull(windowStartup);
+
+        if (handler.PlatformView is not UIWindow uiWindow)
+            ArgumentNullException.ThrowIfNull(nameof(UIWindow));
+        else
+            _PlatformWindow = uiWindow;
+
         _Window = window;
         _WindowStartup = windowStartup;
     }
 
     readonly Window _Window;
     readonly WindowStartup _WindowStartup;
+    readonly UIWindow? _PlatformWindow;
 
 
     bool IService.Run()
     {
-
+        
         return true;
     }
 
@@ -39,8 +51,5 @@ internal partial class WindowStartupService : IWindowStartupService, IService
         return true;
     }
 
-    bool IWindowStartupService.SetBackdropsKind(BackdropsKind kind)
-    {
-        return true;
-    }
+   
 }
